befc054c0ec217015cbf4b76fedc7149a4e21e0a
WashUMRC <40181349+WashUMRC@users.noreply.github.com>
2018-08-17 08:54:24 -0500

    Update CalculateMaskVolume.m

--- Analyzing/CalculateMaskVolume.m
+++ Analyzing/CalculateMaskVolume.m
@@ -3,3 +3,4 @@ function [hObject,eventdata,handles] = CalculateMaskVolume(hObject,eventdata,han
 len = length(find(handles.bwContour));
 vol = len * handles.info.SliceThickness^3;
 h = msgbox(['The volume of this mask is ' num2str(vol) 'mm^3']);
+%another thing
